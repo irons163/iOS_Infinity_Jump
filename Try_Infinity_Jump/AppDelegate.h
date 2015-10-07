9c970301dd9628dcee70b12a54da20ee4cf7097e
irons163 <irons@ironsdeMacBook-Air.local>
2015-10-07 11:13:14 +0800

    Initial Commit

---
+++ Try_Infinity_Jump/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Try_Infinity_Jump
+//
+//  Created by irons on 2015/10/7.
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
